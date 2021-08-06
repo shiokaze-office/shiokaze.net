f50592d6400a8657650ce74ead3644773e9bb72f
linyows <linyows@gmail.com>
2021-08-07 00:59:55 +0900

    styled component

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,7 @@ module.exports = {
 
     // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
 
     /**
      * this (optional) plugin enables Progressive Web App + Offline functionality
