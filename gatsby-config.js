8851edee53b7b7dc9f8f236d239864298647a503
linyows <linyows@gmail.com>
2021-08-07 10:34:29 +0900

    fix netlify caches

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,43 +1,18 @@
-/**
- * 👋 Hey there!
- * This file is the starting point for your new WordPress/Gatsby site! 🚀
- * For more information about what this file is and does, see
- * https://www.gatsbyjs.com/docs/gatsby-config/
- *
- */
-
 module.exports = {
-  /**
-   * Adding plugins to this array adds them to your Gatsby site.
-   *
-   * Gatsby has a rich ecosystem of plugins.
-   * If you need any more you can search here: https://www.gatsbyjs.com/plugins/
-   */
   plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
-      /**
-       * First up is the WordPress source plugin that connects Gatsby
-       * to your WordPress site.
-       *
-       * visit the plugin docs to learn more
-       * https://github.com/gatsbyjs/gatsby/blob/master/packages/gatsby-source-wordpress/README.md
-       *
-       */
       resolve: `gatsby-source-wordpress`,
       options: {
-        // the only required plugin option for WordPress is the GraphQL url.
         url:
           process.env.WPGRAPHQL_URL ||
           `https://linyows.lolipop.jp/wp.shiokaze.net/graphql`,
       },
     },
-
-    /**
-     * We need this plugin so that it adds the "File.publicURL" to our site
-     * It will allow us to access static url's for assets like PDF's
-     *
-     * See https://www.gatsbyjs.org/packages/gatsby-source-filesystem/ for more info
-     */
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -45,37 +20,63 @@ module.exports = {
         path: `${__dirname}/content/assets`,
       },
     },
-
-    /**
-     * The following two plugins are required if you want to use Gatsby image
-     * See https://www.gatsbyjs.com/docs/gatsby-image/#setting-up-gatsby-image
-     * if you're curious about it.
-     */
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-
     {
-      // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter WordPress Blog`,
-        short_name: `GatsbyJS & WP`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        name: `行政書士 しおかぜ事務所`,
+        short_name: 'しおかぜ事務所',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: `standalone`,
+        icon: `content/assets/shiokaze-icon.png`,
       },
     },
-
-    // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-styled-components`,
-
-    /**
-     * this (optional) plugin enables Progressive Web App + Offline functionality
-     * To learn more, visit: https://gatsby.dev/offline
-     */
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-plugin-netlify',
+      options: {
+        headers: {
+          '/*.html': [
+            'cache-control: public, max-age=0, must-revalidate'
+          ],
+          '/page-data/app-data.json': [
+            'cache-control: public, max-age=0, must-revalidate'
+          ],
+          '/page-data/*': [
+            'cache-control: public, max-age=0, must-revalidate'
+          ],
+          '/static/*': [
+            'cache-control: public, max-age=31536000, immutable'
+          ],
+          '/icons/*': [
+            'cache-control: public, max-age=31536000, immutable'
+          ],
+          '/media/*': [
+            'cache-control: public, max-age=31536000, immutable'
+          ],
+          '/sw.js': [
+            'cache-control: public, max-age=0, must-revalidate'
+          ],
+          '/**/*.js': [
+            'cache-control: public, max-age=31536000, immutable'
+          ],
+          '/**/*.css': [
+            'cache-control: public, max-age=31536000, immutable'
+          ],
+        }
+      }
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#CEC0D9`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-10693153-3`,
+      },
+    },
   ],
 }
